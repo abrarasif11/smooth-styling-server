cb3c84f3c93d6ad61384933c9b5e3c3a6cac5469
abrarasif11 <faasif929@gmail.com>
2023-01-21 18:59:53 +0600

    connected-to-mongo

--- index.js
+++ index.js
@@ -11,7 +11,28 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Smooth Styling Server is Running')
 })
+// Mongo Collection //
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vhdpi0m.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    const categoryCollection = client.db('smoothStyling').collection('categories');
+    try {
+        app.get('/categories', async (req, res) => {
+            const query = {}
+            const cursor = categoryCollection.find(query);
+            const categories = await cursor.toArray();
+            res.send(categories);
+        });
+    }
+
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
 app.listen(port, () => {
     console.log(`Smooth Styling Server is Running on ${port}`);
 })
